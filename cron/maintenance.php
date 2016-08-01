97868de00bebf9f77a17eadb4abb9eceb7538cb5
daniel-silver <estudiosima@gmail.com>
2016-08-01 21:03:52 +0000

    Removed db info. Changing age on year change.

--- cron/maintenance.php
+++ cron/maintenance.php
@@ -4,16 +4,21 @@ include '../lib/common.php';
 
 $sql = 'SELECT su.id, su.email, su.first_name FROM site_users su LEFT JOIN site_users_status st ON (su.site_users_status = st.id) WHERE st.key = "approved" AND su.notified != "Y" ';
 $result = db_query_array($sql);
-
 if ($result) {
 	foreach ($result as $row) {
 		$info = array();
 		$info['first_name'] = $row['first_name'];
 		$email = SiteEmail::getRecord('usuario-aprobado');
-		print_r($row);
-		print_r($email);
 		Email::send($CFG->contact_email,$row['email'],$email['title'],$CFG->email_smtp_send_from,false,$email['content'],$info);
 		db_update('site_users',$row['id'],array('notified'=>'Y'));
 	}
 }
+
+$m1 = date('Y',time());
+$m2 = date('Y',strtotime('-6 minute'));
+if ($m1 != $m2) {
+	$sql = 'UPDATE site_users SET age = age + 1 WHERE age > 0';
+	db_query($sql);
+}
+
 ?>
